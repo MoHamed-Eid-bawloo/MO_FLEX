b49b9b706a9df4b721487b6393743800afe3ebdd
Mohamed Eid <162780854+MoHamed-Eid-bawloo@users.noreply.github.com>
2025-01-18 20:43:35 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,6 @@ const app = express();
 
 //middlewares
 app.use(express.json({ limit: "30mb" }));
-app.use(cors({
-    origin: ["https://hcmovieapp.netlify.app", "http://localhost:5173"],
-    credentials: true,
-}));
 app.use(cookieParser());
 
 //routes
