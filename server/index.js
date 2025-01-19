1db5f3d0de24850ebdbbdb775079c66b2b0a45f7
Mohamed Eid <162780854+MoHamed-Eid-bawloo@users.noreply.github.com>
2025-01-19 02:18:12 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ const app = express();
 
 //middlewares
 app.use(express.json({ limit: "30mb" }));
-app.options("*", cors({
-    origin: "https://mo-flex-pe7k.vercel.app",
-    credentials: true
+app.use(cors({
+    origin: ["https://mo-flex-pe7k.vercel.app", "http://localhost:5173"],
+    credentials: true,
 }));
 app.use(cookieParser());
 
