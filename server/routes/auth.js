af4b5fb5db14b8673e21f70f22ea4272457fae20
Mohamed Eid <162780854+MoHamed-Eid-bawloo@users.noreply.github.com>
2025-01-19 02:23:55 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,9 @@
 import express from "express"
-import { signUp, logIn, logOut, googleSignIn } from "../controllers/auth.js";
+import { signUp, logIn, logOut } from "../controllers/auth.js";
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
 router.get("/logout", logOut);
-router.post("/google", googleSignIn);
 
 export default router;
